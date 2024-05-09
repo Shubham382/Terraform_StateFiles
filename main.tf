2dcbb8781cb0d9274f7396bf3e914038a8c2319d
Shubham <shubhammandavkar95@gmail.com>
2024-05-09 15:33:53 +0530

    --- Auto Git Commit ---

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 
-# interact with cloud provider such as AWS
-
-provider "aws" {  
-    profile = "default"
-    region = "ap-south-1" 
-}
-
 # 1] Create VPC 
 resource "aws_vpc" "my-vpc" {
     cidr_block = "10.0.0.0/16"
